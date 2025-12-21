2000128f236a89423a6b049156e31c90353a17b1
Bergbauer22 <dieterzeller24@gmail.com>
2025-12-21 13:24:54 +0100

    Merge pull request #1 from Bergbauer22/master
    Release V.1.0.0 for Minecraft Version 1.21.2

--- src/main/java/net/bergbauer/better_pvp/BetterPvP.java
+++ src/main/java/net/bergbauer/better_pvp/BetterPvP.java
@@ -11,7 +11,7 @@ public class BetterPvP implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("BetterPvP loaded successfully");
+		LOGGER.info("BetterPvP loaded successfully :-)");
 		PlayerColorLoader.loadUserColors(PlayerColorLoader.filePath);
 	}
 }
